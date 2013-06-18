83ff4b5798d8bab3dea4198cbab973f44525c36e
CloudDreamer <dashtherainbow37@gmail.com>
2013-06-18 22:11:23 +0200

    added a simple Programm/state code to Programm & State

--- TuringSB/src/algo/Programm.java
+++ TuringSB/src/algo/Programm.java
@@ -3,16 +3,45 @@ package algo;
 public class Programm {
 
 	State def = new State();
+	State back = new State();
+	State parse = new State();
+	State count = new State();
+	State check = new State();
+	
+	int currentState = 0;
 	
 	public Programm(){
+		String[] bp = {"goto check"};
+		String[] bpn = {"l"};
+		back.init("#", bp, bpn);
+		
+		String[] pp = {"write #", "goto count"};
+		String[] ppn = {"r"};
+		parse.init("a", pp, ppn);
+		
+		String[] cp = {"r", "add 1", "back"};
+		String[] cpn = {"r"};
+		count.init("*", bp, bpn);
+		
+		String[] chp = {"END"};
+		String[] chpn = {"goto Parse"};
+		check.init("#", chp, chpn);
 		
 	}
 	
-	public String sendNext(){
-		String back = "";
-		
-		
-		return back;
+	public String[] sendNext(String parseIn){
+		String[] defa = {""};
+		switch (currentState){
+		case 0: {return back.giveNext(parseIn);}
+		case 1: {return parse.giveNext(parseIn);}
+		case 2: {return count.giveNext(parseIn);}
+		case 3: {return check.giveNext(parseIn);}
+		default: {return defa;}
+		}
+	}
+	
+	public void changeState(int i){
+		currentState = i;
 	}
 	
 }
--- TuringSB/src/algo/State.java
+++ TuringSB/src/algo/State.java
@@ -2,10 +2,28 @@ package algo;
 
 public class State {
 	
-	
+	String c;
+	String t;
+	String[] p;
+	String[] pn;
 	
 	public State(){
 		
 	}
 
+	public void init(String t, String[] p, String[] pn){
+		this.t = t;
+		this.p = p;
+		this.pn = pn;
+	}
+	
+	public String[] giveNext(String parse){
+		if(parse == t){
+			return p;
+		}
+		else{
+			return pn;
+		}
+	}
+	
 }
