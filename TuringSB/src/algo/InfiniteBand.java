aef0a5a9b703b79656907dd4974f88a779af81ba
CloudDreamer <dashtherainbow37@gmail.com>
2013-06-18 16:16:10 +0200

    Modified Infinite Band - added basic methods

--- TuringSB/src/algo/InfiniteBand.java
+++ TuringSB/src/algo/InfiniteBand.java
@@ -1,9 +1,32 @@
 package algo;
 
-public class InfiniteBand {
+import java.util.LinkedList;
 
+public class InfiniteBand {
+	
+	LinkedList b = new LinkedList();
+	
 	public InfiniteBand(){
-		
+		b.add("#");
+		b.add("*");
+	}
+	
+	public void add(String a){
+		b.add(a);
+	}
+	
+	public void set(int i, String a){
+		b.set(i, a);
+	}
+	
+	public void remove(int i){
+		b.remove(i);
+	}
+	
+	public String get(int i){
+		String t;
+		t = (String) b.get(i);
+		return t;
 	}
 	
 }
