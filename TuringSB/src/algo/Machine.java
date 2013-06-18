62506340277bb704c2be41876f7eb3ca0739fc63
CloudDreamer <dashtherainbow37@gmail.com>
2013-06-18 22:16:07 +0200

    commented TO-DO in machine

--- TuringSB/src/algo/Machine.java
+++ TuringSB/src/algo/Machine.java
@@ -6,7 +6,9 @@ public class Machine {
 	Programm p = new Programm();
 	
 	public Machine(){
-		
+		//TO-DO: parse in the retured next files -> split string up
+		//		 create a head!!
+		//		 switch the code, e.g. l makes the head go left, add adds a param to the current...
 	}
 	
 }
